6c61cf72cffd79fead4849bd1d67ab0cd01d0b41
Josh Cummings <joshcummingsdesign@gmail.com>
2023-10-23 18:51:55 -0700

    Fix build error

--- src/components/DrawDot/generateGraphSvg.ts
+++ src/components/DrawDot/generateGraphSvg.ts
@@ -1,3 +1,5 @@
+/* eslint-disable-next-line  @typescript-eslint/ban-ts-comment */
+// @ts-ignore
 import { instance } from "@viz-js/viz";
 import { Value } from "../../engine/Value";
 import { buildDot } from "./buildDot";
